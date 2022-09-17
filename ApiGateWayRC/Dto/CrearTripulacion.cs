39967855f22fea40be10e84bd7ba79294ef394e6
RcerrutoBsol <ronald.cerruto@gmail.com>
2022-09-17 09:12:49 -0400

    ok
    ok

---
+++ ApiGateWayRC/Dto/CrearTripulacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ApiGateWayRC.Dto
+{
+    public class CrearTripulacion
+    {
+        public string TripulacionNombre { get; set; }
+        public int TripulacionEstado { get; set; }
+        public string TripulacionGuid { get; set; }
+
+        public Guid GUID { get; set; }
+    }
+}
